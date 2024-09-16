d102654264311c8f2ccd09189cfbf2fc7f7ff874
Ahmed Idiris <aamedidris1999@gmail.com>
2024-09-16 14:05:46 +0200

    new components

---
+++ src/components/Login/Login.stories.jsx
@@ -0,0 +1,14 @@
+import Login from "./Login";
+
+export default {
+  title: 'FRK/components/Login',
+  component: Login,
+}
+
+export const Default = {
+
+}
+
+export const BreakingMyComponent = {
+
+}
